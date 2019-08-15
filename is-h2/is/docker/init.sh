96cf8a96297e958214871de5d32a1a55f5952056
anupamgogoi-wso2 <anupam@wso2.com>
2019-08-15 19:04:33 -0300

    iii

---
+++ is-h2/is/docker/init.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+set -e
+
+docker_container_ip=$(awk 'END{print $1}' /etc/hosts)
+echo "#######Docker container IP#######" + ${docker_container_ip}
+sed -i "s#<parameter\ name=\"localMemberHost\".*<\/parameter>#<parameter\ name=\"localMemberHost\">${docker_container_ip}<\/parameter>#" ${WSO2_SERVER_HOME}/repository/conf/axis2/axis2.xml
+sh ${WSO2_SERVER_HOME}/bin/wso2server.sh "$@"
