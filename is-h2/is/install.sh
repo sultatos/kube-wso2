60dab39a7a6ee87ff606588bcf71dbec031a1cf9
anupamgogoi-wso2 <anupam@wso2.com>
2019-08-20 10:59:32 -0300

    in

--- is-h2/is/install.sh
+++ is-h2/is/install.sh
@@ -4,6 +4,11 @@
 oc login -u developer -p developer
 oc new-project wso2
 
+# Docker
+docker login -u developer -p $(oc whoami -t) 172.30.1.1:5000
+docker tag is 172.30.1.1:5000/wso2/is
+docker push 172.30.1.1:5000/wso2/is
+
 # Create service account.
 oc create serviceaccount wso2svc-account -n wso2
 
@@ -21,11 +26,3 @@ oc apply -f kube/is-route.yaml -n wso2
 
 # Create deployment
 oc apply -f kube/is-deployment.yaml -n wso2
-
-
-
-# Utils
-alias dc="oc get dc"
-alias pods="oc get pods"
-alias svc="oc get svc"
-alias cm="oc get cm"
